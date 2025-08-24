cd17ac656066edbe0568ab2c47a17f8d016bd707
Deepesh70 <deepeshdangi700@gmail.com>
2025-08-24 14:08:36 +0530

    added cocktail section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,29 @@
-import { navLinks} from '../../constants/index.js'
+import { navLinks } from '../../constants/index.js';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 const Navbar = () => {
 
-    useGSAP(()=> {
+    useGSAP(() => {
+        gsap.registerPlugin(ScrollTrigger);
         const navTween = gsap.timeline({
             scrollTrigger: {
                 trigger: "nav",
-                start: "bottom top"
+                start: "bottom top",
+                toggleActions: "play none none reverse"
             }
         });
-        navTween.fromTo('nav', {backgroundColor: 'transparent'}, 
-            {backgroundColor: '#00000050',
-                backgroundFilter:'blur(10px)',
-                duration : 1,
+        navTween.fromTo('nav',
+            { backgroundColor: 'transparent', backdropFilter: 'blur(0px)' },
+            {
+                backgroundColor: '#00000050',
+                backdropFilter: 'blur(10px)',
+                duration: 1,
                 ease: 'power1.inOut'
-            });   
-    })
+            }
+        );
+    }, []);
 
 
   return (
