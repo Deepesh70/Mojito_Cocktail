923b780757fe5bc79b3846afffe6aba6ad6afabb
Deepesh70 <deepeshdangi700@gmail.com>
2025-08-27 21:49:57 +0530

    Contact section added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Cocktail from "./components/cocktail";
 import About from "./components/About";
 import Art from "./components/Art";
 import Menu from "./components/Menu";
+import Contact from "./components/Contact";
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -18,6 +20,7 @@ const App = () => {
             <About />
             <Art />
             <Menu />
+            <Contact />
         </main>
     )
 }
